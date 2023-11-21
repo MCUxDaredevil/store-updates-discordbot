153d2e144bc82c859e58627d3c44d399a30c077c
Aryan Donga (MCUxDaredevil) <daryan6680@proton.me>
2023-11-21 22:29:08 +0530

    Initialize projects

---
+++ discord-bot/index.js
@@ -0,0 +1,18 @@
+const {Client, GatewayIntentBits} = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
